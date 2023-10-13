930fd8be2d96eda266d6cd99cd4bce7704309bfd
Akhil Naidu <kaparapu.akhilnaidu@gmail.com>
2023-10-13 11:51:02 +0530

    Contentql backend (#1)
    * init contentql backend
    
    * chore: config sync changes and .env.example
    
    ---------
    
    Co-authored-by: girish chalumuri <girishchalumuri@gmail.com>

---
+++ src/api/homep/controllers/homep.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * homep controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::homep.homep');
---
+++ src/api/homep/services/homep.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * homep service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::homep.homep');
