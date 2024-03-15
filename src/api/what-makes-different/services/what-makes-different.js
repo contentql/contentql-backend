eb1916bcb6d6dbd8b2b307c935984b54e0274673
charanm927 <charanm927@gmail.com>
2024-03-15 21:17:49 +0530

    chore: "What make different type added"

---
+++ src/api/what-makes-different/services/what-makes-different.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * what-makes-different service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::what-makes-different.what-makes-different');
